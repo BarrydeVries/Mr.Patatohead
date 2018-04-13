3e9b4dc9a223b5ca89985cce1d9377b40cde7c99
barry <barry.de.vries123@gmail.com>
2018-04-13 20:48:01 +0200

    Werkende versie

--- app/src/main/java/com/example/barry/mrpatatohead/MainActivity.java
+++ app/src/main/java/com/example/barry/mrpatatohead/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.barry.mrpatatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void checkClicked(View v) {
+        CheckBox checkbox = (CheckBox) v;
+
+        String name = checkbox.getText().toString();
+        int imageNo = getResources().getIdentifier(name, "id", getPackageName());
+
+        ImageView image = findViewById(imageNo);
+
+        if (checkbox.isChecked()) {
+            image.setVisibility(View.VISIBLE);
+        } else {
+            image.setVisibility(View.INVISIBLE);
+        }
+
+    }
 }
+
